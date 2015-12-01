5f9c86df7f34766c6be1b3f21518f5e50cba2085
Liang_Tang <ltangmt@gmail.com>
2015-12-01 00:58:28 -0500

    modifying: svm_regression function

--- Matlab/svm_regression.m
+++ Matlab/svm_regression.m
@@ -1,4 +1,5 @@
-function model = svm_regression(y, x)
-%     tic;model = svmtrain(y, x, ['-s 3 -t 2 -n ' num2str(0.5) ' -c ' num2str(1)]);toc;
-    model = svmtrain(y, x, '-s 4');
+function estimated_result = svm_regression(y, x)
+    tic;model = svmtrain(y, x, ['-s 3 -t 2 -n ' num2str(0.5) ' -c ' num2str(1)]);toc;
+%     model = svmtrain(y, x, '-s 4');
+    tic;estimated_result = svmpredict(y,x,model);
 end
