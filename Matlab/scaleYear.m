5ec64302a01c7f6b9626d4364d62ac6b3069ea68
Liang_Tang <ltangmt@gmail.com>
2015-12-01 00:49:51 -0500

    adding scaleYear function to scale year value into range from 0 to 1

--- Matlab/scaleYear.m
+++ Matlab/scaleYear.m
@@ -1,4 +1,16 @@
-function scaled_year = scaleYear(year, original_max, original_min, new_max, new_min)
-scaled_year = ((( year - original_min) * (new_max - new_min)) / (original_max - original_min)) + new_min;
+% =========================================================================
+% This function scale the year value into the range from 0 to 1
+% =========================================================================
+% @param year: the year value that would be scaled
+% =========================================================================
+% @retval scaled_year: year value in the range from 0 to 1
+
+function scaled_year = scaleYear(year)
+years = cell2mat(year);
+original_max = max(years);
+original_min = min(years);
+new_max = 1;
+new_min = 0;
+scaled_year = ((( years - original_min) * (new_max - new_min)) / (original_max - original_min)) + new_min;
 
 end
