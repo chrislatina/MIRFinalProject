48a0edff1b32d822707f5e05f4f00fffab10784f
Liang_Tang <ltangmt@gmail.com>
2015-12-02 19:56:31 -0500

    svm_regression function: using three parameters as input and returning estimated result

--- Matlab/svm_regression.m
+++ Matlab/svm_regression.m
@@ -1,5 +1,15 @@
-function estimated_result = svm_regression(y, x)
-    tic;model = svmtrain(y, x, ['-s 3 -t 2 -n ' num2str(0.5) ' -c ' num2str(1)]);toc;
-%     model = svmtrain(y, x, '-s 4');
-    tic;estimated_result = svmpredict(y,x,model);
+% =========================================================================
+% This function use the svm as the model of regression
+% =========================================================================
+% @param train_data
+% @param train_label
+% @param test_data
+% =========================================================================
+% @retval estimated_result: values are between 0 and 1 which indicate the
+% predicted year
+
+
+function estimated_result = svm_regression(train_data, train_label, test_data)
+    tic;model = svmtrain(train_label, train_data, ['-s 3 -t 2 -n ' num2str(0.5) ' -c ' num2str(1)]);toc;
+    tic;estimated_result = svmpredict(test_data, model);
 end
